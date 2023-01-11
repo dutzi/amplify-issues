53e48cbe8d92e859bf5199f77fdecfc66cebf7b1
Eldad Bercovici <eldad@deepdub.ai>
2023-01-11 10:20:18 +0200

    init commit
    anything you see here was created by following Amplify's Getting Started Guide

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyissues": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
